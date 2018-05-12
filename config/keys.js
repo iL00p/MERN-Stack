aa5f17c4a5f84f1a3fb6c805d664bf206c069ab1
Arjun Sreedhar <arjun.sreedhar@experionglobal.com>
2018-05-12 09:38:54 +0530

    Implemented login and jwt token auth.

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI : 'mongodb://mongostudy:mongostudy@ds119650.mlab.com:19650/mongo_study'
+  mongoURI : 'mongodb://mongostudy:mongostudy@ds119650.mlab.com:19650/mongo_study',
+  secret : 'secret',
 };
