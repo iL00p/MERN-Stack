f032db7810c295c4f97d85fb28d7a77ad70bbe70
Arjun Sreedhar <arjun.sreedhar@experionglobal.com>
2018-05-29 13:28:19 +0530

    Setup landing page.

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import Navbar from './components/layouts/Navbar';
+import Footer from './components/layouts/Footer';
+import Landing from './components/layouts/Landing';
 
 class App extends Component {
   render() {
     return (
-        <h1>My React App</h1>
+        <Fragment>
+          <Navbar/>
+          <Landing/>
+          <Footer/>
+        </Fragment>
     );
   }
 }
